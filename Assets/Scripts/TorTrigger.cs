02e5e91cbb46e5646efb5cfa5845ca8cde6dda14
Julian Heinken <hi@julianheinken.com>
2025-05-20 16:15:09 +0200

    Unity 04 (Lösung mehrere Lichter)

--- Assets/Scripts/TorTrigger.cs
+++ Assets/Scripts/TorTrigger.cs
@@ -4,9 +4,11 @@ public class TorTrigger : MonoBehaviour
 {
     public GameObject tor;
     public Light alarmLicht;
+    public Light alarmLicht2;
     private void OnTriggerEnter(Collider other)
     {
         tor.SetActive(false);
         alarmLicht.color = Color.green;
+        alarmLicht2.color = Color.yellow;
     }
 }
